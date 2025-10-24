e9f2e7da76a98edf2b5566f2e5c721bb743006fe
lukezheng2011 <lukezheng2011@gmail.com>
2025-10-24 09:12:40 -0400

    a
    a

--- idiot.py
+++ idiot.py
@@ -30,24 +30,28 @@ def gamble(a,b,c,d):
                 a = a - 2
                 remainder = 0
         
-
+        a -= 1
+        amount += 1
         
         if b - 35 > 0:
             a = a +30
+            a -= 1
+            amount += 1
            # total = total + 30
         if c - 100 > 0:
             a = a + 60
+            a -= 1
+            amount += 1
             #total = total + 60
         if d - 10 > 0:
             a = a + 9
+            a -= 1
+            amount += 1
            # total = total + 9
             
         break
-    amount = 152
 
     print(f"Martha can play {amount} times before going broke")
 
 gamble(77,4,9,3)
 #gamble(int(input("How many quarters do you have: ")),int(input("How many times has machine one been used?")),int(input("How many times has machine two been used?")),int(input("How many times has machine three been used?")))
-
-
