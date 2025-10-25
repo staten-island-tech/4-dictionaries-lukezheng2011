26f377a4a1af64ebaf5e37336c1b599765264e06
lukezheng2011 <lukezheng2011@gmail.com>
2025-10-24 20:33:12 -0400

    a
    a

--- idiot.py
+++ idiot.py
@@ -1,57 +1,36 @@
-def gamble(a,b,c,d):
-    remainder=0
-    each = 0
-    total = 0
-    amount = 0
-
-    while a>=0:
-        for i in range(2):
-            if a % 3 == i:
-                
-                remainder = i
-                each = (a - remainder)/3
-                b = b+each
-                c = c+each
-                d = d+each
-                
-                
-                a = a - (3 * each)
-                amount += a + (3*each)
-                total = total + amount
-
-                
-            if remainder == 1:
-                b = b + 1
-                a = a-1
-                remainder = 0
-            elif remainder == 2:
-                b = b + 1
-                c = c +1
-                a = a - 2
-                remainder = 0
-        
+def gamble(a, b, c, d):
+    plays = 0
+    
+    while a > 0:
+        # Machine 1
+        plays += 1
         a -= 1
-        amount += 1
+        b += 1
+        if b == 35:
+            a += 30
+            b = 0
+        if a == 0:
+            break
         
-        if b - 35 > 0:
-            a = a +30
-            a -= 1
-            amount += 1
-           # total = total + 30
-        if c - 100 > 0:
-            a = a + 60
-            a -= 1
-            amount += 1
-            #total = total + 60
-        if d - 10 > 0:
-            a = a + 9
-            a -= 1
-            amount += 1
-           # total = total + 9
-            
-        break
-
-    print(f"Martha can play {amount} times before going broke")
-
-gamble(77,4,9,3)
-#gamble(int(input("How many quarters do you have: ")),int(input("How many times has machine one been used?")),int(input("How many times has machine two been used?")),int(input("How many times has machine three been used?")))
+        # Machine 2
+        plays += 1
+        a -= 1
+        c += 1
+        if c == 100:
+            a += 60
+            c = 0
+        if a == 0:
+            break
+        
+        # Machine 3
+        plays += 1
+        a -= 1
+        d += 1
+        if d == 10:
+            a += 9
+            d = 0
+        if a == 0:
+            break
+    
+    print(f"Martha can play {plays} times before going broke.")
+gamble(int(input("How many quarters does Martha have: ")),int(input("How much has machine one been used: ")),int(input("How much has machine two been used: ")),int(input("How much has machine three been used: ")))
